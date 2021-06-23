274bf902e5c79b37392832bda122c0b08ff79b0f
Shoma FUKUDA <fkshom@gmail.com>
2021-06-23 20:40:54 +0900

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,75 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <div v-for="(item, index) in data" :key="index">
+    {{index}}
+    <div v-for="(rule, index) in item.rules" :key="index">
+     {{index}}{{rule}}
+    </div>
+  </div> -->
+   <div>
+    <Multiselect
+      v-model="selected_srchosts"
+      mode="tags"
+      placeholder="Select your characters"
+      :options="srchosts()"
+      :searchable="true"
+    />
+    <Multiselect
+      v-model="selected_dsthosts"
+      mode="tags"
+      placeholder="Select your characters"
+      :options="dsthosts()"
+      :searchable="true"
+    />
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import Multiselect from '@vueform/multiselect'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Multiselect
+  },
+  data() {
+    return {
+      data: [],
+      selected_srchosts: null,
+      selected_dsthosts: null,
+    }
+  },
+  created() {
+    this.getData()
+  },
+  methods: {
+    srchosts() {
+      if(this.data.length == 0){
+        return []
+      } else {
+        return [...new Set(this.data[0].two_tuples.reduce((prev, cur) => {
+          return [...prev, cur[0]]
+        }, []))]
+      }
+    },
+    dsthosts() {
+      if(this.data.length == 0){
+        return []
+      } else {
+        return [...new Set(this.data[0].two_tuples.reduce((prev, cur) => {
+          return [...prev, cur[1]]
+        }, []))]
+      }
+    },
+      getData() {
+        this.axios.get('/acl1.json')
+          .then((response) => {
+            this.data = response.data
+          })
+          .catch((e) => {
+            alert(e);
+          });
+      }
   }
 }
 </script>
@@ -24,3 +84,5 @@ export default {
   margin-top: 60px;
 }
 </style>
+<style src="@vueform/multiselect/themes/default.css"></style>
+
--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
+
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios' //追記
+import VueAxios from 'vue-axios' //追記
+import Multiselect from '@vueform/multiselect'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(VueAxios, axios)
+// app.use(Multiselect, axios)
+app.mount('#app')
