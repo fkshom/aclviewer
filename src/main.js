3399a752d325acc2b2764cf50918eae91422c864
Shoma FUKUDA <fkshom@gmail.com>
2021-06-28 21:30:54 +0900

    wip

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
+
 
 import axios from "axios";
 import VueAxios from "vue-axios";
